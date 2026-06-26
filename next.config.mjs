b6550da4c984d4ecf3e2481fe8375e41c8add2aa
Emmanuelmgc <Emmanuelmgc@users.noreply.github.com>
2026-06-27 00:42:10 +0800

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: isGitHubPages ? "/trueast-site" : "",
+  assetPrefix: isGitHubPages ? "/trueast-site" : "",
+};
 
 export default nextConfig;
